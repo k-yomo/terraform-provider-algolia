4f8613214e58f3b515f5c47bf0e33092aca13115
k-yomo <kanji.yy@gmail.com>
2021-04-25 23:03:06 +0900

    Update algolia_index example

--- README.md
+++ README.md
@@ -39,24 +39,42 @@ provider "algolia" {
 
 resource "algolia_index" "example" {
   name = "example"
-  searchable_attributes = [
-    "title",
-    "category,tag",
-    "unordered(description)",
-  ]
-  attributes_for_faceting = [
-    "category"
-  ]
-  unretrievable_attributes = [
-    "author_email"
-  ]
-  attributes_to_retrieve = [
-    "title",
-    "category",
-    "tag",
-    "description",
-    "body"
-  ]
+  attributes_config {
+    searchable_attributes = [
+      "title",
+      "category,tag",
+      "unordered(description)",
+    ]
+    attributes_for_faceting = [
+      "category"
+    ]
+    unretrievable_attributes = [
+      "author_email"
+    ]
+    attributes_to_retrieve = [
+      "title",
+      "category",
+      "tag",
+      "description",
+      "body"
+    ]
+  }
+
+  ranking_config {
+    ranking = [
+      "words",
+      "proximity"
+    ]
+  }
+
+  faceting_config {
+    max_values_per_facet = 50
+    sort_facet_values_by = "alpha"
+  }
+
+  languages_config {
+    remove_stop_words_for = ["en"]
+  }
 }
 
 resource "algolia_api_key" "example" {
--- examples/resources/algolia_index/resource.tf
+++ examples/resources/algolia_index/resource.tf
@@ -1,30 +1,39 @@
 resource "algolia_index" "example" {
   name = "example"
-  searchable_attributes = [
-    "title",
-    "category,tag",
-    "unordered(description)",
-  ]
-  attributes_for_faceting = [
-    "category"
-  ]
-  unretrievable_attributes = [
-    "author_email"
-  ]
-  attributes_to_retrieve = [
-    "title",
-    "category",
-    "tag",
-    "description",
-    "body"
-  ]
-  ranking = [
-    "words",
-    "proximity"
-  ]
-  replicas = [
-    "replica_test1",
-    "replica_test2",
-  ]
-  max_values_per_facet = 50
+  attributes_config {
+    searchable_attributes = [
+      "title",
+      "category,tag",
+      "unordered(description)",
+    ]
+    attributes_for_faceting = [
+      "category"
+    ]
+    unretrievable_attributes = [
+      "author_email"
+    ]
+    attributes_to_retrieve = [
+      "title",
+      "category",
+      "tag",
+      "description",
+      "body"
+    ]
+  }
+
+  ranking_config {
+    ranking = [
+      "words",
+      "proximity"
+    ]
+  }
+
+  faceting_config {
+    max_values_per_facet = 50
+    sort_facet_values_by = "alpha"
+  }
+
+  languages_config {
+    remove_stop_words_for = ["en"]
+  }
 }
