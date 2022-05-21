38dc273e102108c25c73237e4bfd4cb64d4a1d9b
k-yomo <kanji.yy@gmail.com>
2022-05-22 04:32:40 +0900

    Fix virtual replica index example

--- examples/resources/algolia_virtual_index/resource.tf
+++ examples/resources/algolia_virtual_index/resource.tf
@@ -1,41 +1,12 @@
 resource "algolia_index" "example" {
   name = "example"
+
   attributes_config {
     searchable_attributes = [
       "title",
-      "category,tag",
-      "unordered(description)",
-    ]
-    attributes_for_faceting = [
-      "category"
-    ]
-    unretrievable_attributes = [
-      "author_email"
-    ]
-    attributes_to_retrieve = [
-      "title",
-      "category",
-      "tag",
       "description",
-      "body",
-      "likes"
     ]
-  }
-
-  ranking_config {
-    ranking = [
-      "words",
-      "proximity"
-    ]
-  }
-
-  faceting_config {
-    max_values_per_facet = 50
-    sort_facet_values_by = "alpha"
-  }
-
-  languages_config {
-    remove_stop_words_for = ["en"]
+    attributes_to_retrieve = ["*"]
   }
 
   replicas = ["virtual(example_replica)"]
@@ -48,12 +19,7 @@ resource "algolia_virtual_index" "example_virtual_replica" {
     unretrievable_attributes = [
       "author_email"
     ]
-    attributes_to_retrieve = [
-      "title",
-      "category",
-      "tag",
-      "likes"
-    ]
+    attributes_to_retrieve = ["*"]
   }
 
   ranking_config {
