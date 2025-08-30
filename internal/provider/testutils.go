875cede8d5b3c23636241b20b806ce466d5f1170
Grégoire Paris <postmaster@greg0ire.fr>
2025-08-30 09:18:29 +0200

    Migrate to terraform-plugin-testing
    This looks like it could fix the broken tests.

--- internal/provider/testutils.go
+++ internal/provider/testutils.go
@@ -3,8 +3,8 @@ package provider
 import (
 	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-provider-algolia/internal/algoliautil"
 	"github.com/rs/xid"
 )
